8f38e72433067d7d07f4d1830857b5ea44e4ff00
jamiiecb <jamiiecb@googlemail.com>
2010-01-18 19:12:22 +0000

    Support for GMail
    Ignore-this: 2f27bd03d7317b71b2adf9a15c9167c7
    
    darcs-hash:20100118191222-b18dd-f2f27f6b6c5088efc751d60c56dc0885c04f70a8.gz

--- src/server.py
+++ src/server.py
@@ -1,15 +1,62 @@
-class Server():
-  def __init__(   )
+import imaplib
+import smtplib
+import email.parser
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
-  def messages():
+class Mail():
+  def __init__(self, user, password, imapAddress, smtpAddress):
+    pass
 
-  def send(message):
+  def connect(self):
+    pass
 
-  def reply(message, text):
+  def fetch(self, folder='Inbox'):
+    parser = email.parser.Parser()
+    self.imap.select(folder)
+    status, uids = self.imap.search(None, 'UNSEEN')
+    uids = uids[0].replace(' ',',')
+    status, data = self.imap.fetch(uids, '(RFC822)')
+    self.imap.close()
+    return [parser.parsestr(mail) for (uid, mail) in data[::2]]
 
-  def log(subject, **args):
+  def send(self, message):
+    smtp.sendmail(self.user, message['To'], message.as_string())  
 
-class GMailServer(Server):
-  def messages():
+  def reply(self, message, text):
+    reply = MIMEMultipart()
+    reply['From'] = self.user
+    reply['To'] = message['From']
+    reply['Subject'] = "RE: " + message['Subject']
+    reply.attach(MIMEText(text))
 
-  def send(message):
+    self.send(reply)
+
+  def log(self, subject, **args):
+    log = MIMEMultipart()
+    log['From'] = self.user
+    log['To'] = 'logaddress'
+    log['Subject'] = "Log: " + subject
+    text = "\n\n".join([k + ":\n" + v for k, v in args])
+    log.attach(MIMEText(text))
+
+    self.send(log)
+
+  def disconnect(self):
+    self.imap.logout()
+    self.smtp.quit()
+
+class GMail(Mail):
+  def __init__(self, user, password):
+    self.user = user
+    self.password = password
+
+  def connect(self):
+    self.imap = imaplib.IMAP4_SSL('imap.gmail.com',993)
+    self.imap.login(self.user, self.password)
+        
+    self.smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    self.smtp.ehlo()
+    self.smtp.starttls()
+    self.smtp.ehlo()
+    self.smtp.login(self.user, self.password)
