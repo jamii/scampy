1cf8ef9e6de8b01d290d5aacacdd2db75d33c7bd
jamiiecb <jamiiecb@googlemail.com>
2010-01-21 15:22:35 +0000

    classifier ready
    Ignore-this: 6f2cf6297ec4b8c1435da0fd4cd3acda
    
    darcs-hash:20100121152235-b18dd-d0e267aba6f16e7dceb725cb5cb2747400e13b7f.gz

--- src/classify.py
+++ src/classify.py
@@ -1,4 +1,22 @@
-import spambayes #?
-from data import classifiers
+import data
+from settings import server
+
+cutoff = 0.95
 
 def classify(text):
+  score, scam = max[(classifier.score(text), scam) for (classifier, scam) in data.classifiers.items()]
+  if score > cutoff:
+    return scam
+  else:
+    return None
+
+def train():
+  for scam in data.scams:
+    msgs = server.fetch(folder=scam)
+    for key, classifier in classifiers.items():
+      for msg in msgs:
+        if key == scam:
+          classifier.train_ham(msg.as_string())
+        else:
+          classifier.train_spam(msg.as_string())
+    # marks msgs as seen
--- src/data.py
+++ src/data.py
@@ -1,14 +1,22 @@
 from os import listdir
 import cPickle as pickle
+from spambayes import hammie
 
-classifiers = []
-responses = []
-replied = set()
+scams = listdir('scams')
+classifiers = {}
+responses = {}
+contacted = set()
+
+def classifier(scam, mode='c'):
+  # Might have to first create the folder/file
+  return hammie.open(('scams/%s/db' % scam), mode)
 
 def load():
-  classifiers = dict( [(scam, ('scams/%s/db' % scam)) for scam in listdir('scams')] )
-  responses = dict( [(scam, listdir(('scams/%s/responses' % scam))) for scam in listdir('scams')] )
-  replied = pickle.load('data/replied')
+  classifiers = dict( [(scam, classifier(scam)) for scam in scams] )
+  responses = dict( [(scam, listdir(('scams/%s/responses' % scam))) for scam in scams] )
+  contacted = pickle.load('data/contacted')
 
 def save():
-  pickle.dump(replied, 'data/replied')
+  for scam, classifier in classifiers:
+    classifier.store()
+  pickle.dump(contacted, 'data/contacted')
